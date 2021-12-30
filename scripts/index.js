66c29f0275566918065ab72227f07c0bcabb9ff1
Pavel Lavrenkov <beasty2006@yandex.ru>
2021-12-30 20:10:37 +0300

    PW5 add initial cards script

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ let nameInput = document.querySelector('.pop-up__field:first-of-type');
 let jobInput = document.querySelector('.pop-up__field:last-of-type');
 let formElement = document.querySelector('.pop-up__form');
 let submitButton = document.querySelector('.pop-up__button-submit');
+
 // скрипт для открытия pop-up и заполнения полей формы текущим значением
 function popUpOpen() {
   nameInput.value = nameProfile.textContent;
@@ -15,11 +16,13 @@ function popUpOpen() {
   popUp.classList.add('pop-up_opened');
 }
 openButton.addEventListener('click', popUpOpen);
+
 // скрипт для закртыия pop-up
 function popUpClose() {
   popUp.classList.remove('pop-up_opened');
 }
 closeButton.addEventListener('click', popUpClose);
+
 // скрипт для заполнения формы
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -28,3 +31,48 @@ function formSubmitHandler(evt) {
   popUpClose();
 }
 submitButton.addEventListener('click', formSubmitHandler);
+
+//скрипт для вывода 6 предустановленных карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function addCard(title, image) {
+  const cardElementTemplate = document.querySelector('#card-element').content;
+  const cardsElementList = document.querySelector('.element-list');
+  const cardElement = cardElementTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = title;
+  cardElement.querySelector('.element__photo').src = image;
+  cardsElementList.prepend(cardElement);
+};
+
+function addArrCards(arr) {
+  arr.forEach(el => {
+    addCard(el.name, el.link);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', addArrCards(initialCards));
