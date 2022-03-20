633e3ecf2da30186bf8e74387391df60d9919536
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-03-20 22:33:41 +0300

    PW8 hotfix v2_1 for review

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,22 @@
 import '../pages/index.css';
 
 // импорты констант
-import { cardsElementSelector } from '../scripts/utils/constnts.js';
-import { openNewCardButton } from '../scripts/utils/constnts.js';
-import { nameSelector } from '../scripts/utils/constnts.js';
-import { jobSelector } from '../scripts/utils/constnts.js';
-import { openProfileButton } from '../scripts/utils/constnts.js';
-import { nameInput } from '../scripts/utils/constnts.js';
-import { jobInput } from '../scripts/utils/constnts.js';
-import { cardSelector } from '../scripts/utils/constnts.js';
-import { initialCards } from '../scripts/utils/constnts.js';
-
-// импорты объектов попапов и форм
-import { popupSettings } from '../scripts/utils/constnts.js';
-import { userInfoPopup } from '../scripts/utils/constnts.js';
-import { newCardPopup } from '../scripts/utils/constnts.js';
-import { imagePopup } from '../scripts/utils/constnts.js';
+import {
+  cardsElementSelector,
+  openNewCardButton,
+  nameSelector,
+  jobSelector,
+  openProfileButton,
+  nameInput,
+  jobInput,
+  cardSelector,
+  initialCards,
+  // импорты настроек попапов
+  popupSettings,
+  userInfoPopup,
+  newCardPopup,
+  imagePopup
+} from '../scripts/utils/constnts.js';
 
 // импорты модулей
 import { Card } from '../scripts/components/Card.js';
@@ -65,43 +66,18 @@ function handleCardClick(name, link) {
   pictureView.open(data);
 }
 
-function cardRender(cardItem) {
-  createCard(cardItem);
-}
-
-const supercards = {
-  items: initialCards,
-  renderer: cardRender([initialCards])
-}
+let dataCards = initialCards;
 
 const cardSection = new Section({
-  items: [],
-  renderer: ({}) => {
+  items: dataCards,
+  renderer: (cardItem) => {
     const newCard = createCard(cardItem);
-    cardSection.createItems(newCard);
+    cardSection.setItem(newCard);
   },
 },
   cardsElementSelector
 );
-cardSection.setItem(initialCards);
-
-
-
-//формирование карточек
-function doSection(items) {
-  const addNewCard = new Section({
-    items,
-    renderer: (cardItem) => {
-      const newCard = createCard(cardItem);
-      addNewCard.setItem(newCard);
-    },
-  },
-    cardsElementSelector
-  );
-  addNewCard.createItems();
-}
-
-doSection(initialCards);
+cardSection.createItems();
 
 //popup для новых карточек
 const popupAddCard = new PopupWithForm(newCardPopupUpgrade, function (data) {
@@ -127,5 +103,6 @@ function handleSubmitNewCardForm(data) {
     name: data.title,
     link: data.info
   };
-  doSection([bigdata]);
+  const newCard = createCard(bigdata);
+  cardSection.setItem(newCard);
 }
